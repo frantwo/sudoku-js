a4e90616c53e2a6bfdb3ff6025539277c9ab0ef1
Francho Garcia <franchog79@gmail.com>
2023-06-15 16:39:19 +0200

    README modificado

--- src/README.cmd
+++ src/README.cmd
@@ -1,5 +1,5 @@
 INFO
-  Algoritmos para resolución de sudokus:
+  Links sobre algoritmos para resolución de sudokus:
     https://es.wikipedia.org/wiki/Algoritmos_para_la_resoluci%C3%B3n_de_sudokus
     https://es.wikipedia.org/wiki/Sudoku_backtracking
 
