8377c3d596ecb8ce532fc0583e02ff781761f87a
Francho Garcia <franchog79@gmail.com>
2023-06-15 16:35:53 +0200

    First commit

---
+++ src/README.cmd
@@ -0,0 +1,6 @@
+INFO
+  Algoritmos para resolución de sudokus:
+    https://es.wikipedia.org/wiki/Algoritmos_para_la_resoluci%C3%B3n_de_sudokus
+    https://es.wikipedia.org/wiki/Sudoku_backtracking
+
+
---
+++ src/components/Board.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Board() {
+  return (
+    <div>
+      <h2>Tablero</h2>
+    </div>
+  )
+}
