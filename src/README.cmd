f077ecbd6371e2e3acdbd72e0aff1e50e1b93893
Francho <francho.garcia@interacso.com>
2023-07-13 16:21:37 +0200

    Added lines to the README to remember myself

--- src/README.cmd
+++ src/README.cmd
@@ -1,6 +1,12 @@
-INFO
-  Links sobre algoritmos para resolución de sudokus:
+#INFO
+  [ ] Links sobre algoritmos para resolución de sudokus:
     https://es.wikipedia.org/wiki/Algoritmos_para_la_resoluci%C3%B3n_de_sudokus
     https://es.wikipedia.org/wiki/Sudoku_backtracking
+  
+  [ ] recurso para implementar el patrón de diseño: OBSERVER
+    https://kinsta.com/es/blog/patrones-de-diseno-javascript/#tipos-de-patrones-de-diseo-de-javascript
+    Breve explicación de su funcionamiento
 
-
+##NOTAS
+  - Después de haber instalado el LINTER veo que hay muchas normas que debería solucionarse si utilizara TypeScript. 
+    Consideraré migrar el proyecto a TypeScript para no tenerme que saltar los mensajes de aviso de tipado.
